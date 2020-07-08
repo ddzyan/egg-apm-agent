cf1e3779ccf33af11b19b4e4871b1f49bbf942cc
ddz <ddzyan@163.com>
2020-07-08 17:04:13 +0800

    完善内容

--- README.md
+++ README.md
@@ -1,8 +1,51 @@
 ## 简介
-使用 elastic-apm-node 完成对 egg 路由链路日志的上传
+使用 elastic-apm-node 封装 egg 插件，完成对请求路由链路日志的上传elk+apm平台，分析链路中逻辑消耗的时间
 
 ### 完成功能
 1. 应用路由日志上传
 2. 应用错误上传
 
 ### 使用
+
+添加插件 config/plugin.js
+```json
+module.exports = {
+ eggApmAgent: {
+    enable: true,
+    package: 'egg-apm-agent',
+  }
+}
+```
+
+修改 package.json ,添加 apm 服务名称和服务地址，并且在egg之前加载apm-agent模块
+```json
+{
+  "scripts":{
+    "debug": "ELASTIC_APM_SERVICE_NAME=test12 ELASTIC_APM_SERVER_URL='http://10.10.0.130:8200' egg-bin debug --require=egg-apm-agent/apm-register.js",
+  }
+}
+```
+
+添加 /app/middleware/apmRouter.js , 定义 transaction 名称，可以在这里修改 transaction 显示逻辑
+```js
+'use strict';
+
+module.exports = () => {
+  return async (ctx, next) => {
+    ctx.app.apm.setTransactionName(ctx.method + ' ' + ctx.request.body.query); // fix unknown router
+    await next();
+  };
+};
+
+```
+
+添加 app.js 加载中间件
+```js
+'use strict';
+
+module.exports = app => {
+  // 中间件，修正apm transaction unknown router
+  app.config.middleware.unshift('apmRouter');
+};
+
+```
--- instrumentation/egg-router.js
+++ instrumentation/egg-router.js
@@ -1,17 +1,39 @@
 'use strict';
-const { Transport } = require('egg-logger');
 
-module.exports = class ApmErrorTransport extends Transport {
-  constructor(options, apm) {
-    super(options);
-    this.apm = apm;
-  }
+const shimmer = require('elastic-apm-node/lib/instrumentation/shimmer');
 
-  log(_level, args) {
-    if (!args[0]) {
-      return;
-    }
-    this.apm.captureError(args[0]); // 上传到apm
-  }
+module.exports = function(Router, agent, { version, enabled }) {
+  if (!enabled) return Router;
 
+  agent.logger.debug('shimming koa-router prototype.match function', version);
+  shimmer.wrap(Router.prototype, 'match', function(orig) {
+    return function(_, method) {
+      const matched = orig.apply(this, arguments);
+
+      if (typeof method !== 'string') {
+        agent.logger.debug('unexpected method type in koa-router prototype.match: %s', typeof method);
+        return matched;
+      }
+
+      if (Array.isArray(matched && matched.pathAndMethod)) {
+        const layer = matched.pathAndMethod.find(function(layer) {
+          return layer && layer.opts && layer.opts.end === true;
+        });
+
+        const path = layer && layer.path;
+        if (typeof path === 'string') {
+          const name = method + ' ' + path;
+          agent._instrumentation.setDefaultTransactionName(name);
+        } else {
+          agent.logger.debug('unexpected path type in koa-router prototype.match: %s', typeof path);
+        }
+      } else {
+        agent.logger.debug('unexpected match result in koa-router prototype.match: %s', typeof matched);
+      }
+
+      return matched;
+    };
+  });
+
+  return Router;
 };
