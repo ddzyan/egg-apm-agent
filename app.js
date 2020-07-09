86830cf22c311db7e7469dbdb6fc1672d72cec83
ddz <ddzyan@163.com>
2020-07-09 10:47:28 +0800

    优化

--- app.js
+++ app.js
@@ -3,11 +3,31 @@ const apm = require('./apm-register');
 
 const ApmErrorTransport = require('./lib/apm-error-transport');
 
-module.exports = app => {
-  app.beforeStart(async () => {
-    // 目前只有在middleware -> apmRouter 里面使用到 app.apm
-    app.apm = apm;
+class AppBootHook {
+  constructor(app) {
+    this.app = app;
+  }
+
+  configWillLoad() {}
+
+  configDidLoad() {}
+
+  async didLoad() {}
+
+  async willReady() {
+    // All plugins have started, can do some thing before app ready
+    this.app.apm = apm;
     // 设置错误日志的传输通道。所有logger.error的错误，都会发送到APM上面。
     app.getLogger('errorLogger').set('apmError', new ApmErrorTransport({ level: 'ERROR' }, apm));
-  });
-};
+  }
+
+  async didReady() {}
+
+  async serverDidReady() {}
+
+  async beforeClose() {
+    // Do some thing before app close.
+  }
+}
+
+module.exports = AppBootHook;
