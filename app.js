bdc6d49cf3134d7c4b4308ffa776bd4ee67011d2
ddz <ddzyan@163.com>
2020-07-13 10:41:08 +0800

    fix: 修复app.js中修改错误日志输出对象bug

--- app.js
+++ app.js
@@ -8,25 +8,11 @@ class AppBootHook {
     this.app = app;
   }
 
-  configWillLoad() {}
-
-  configDidLoad() {}
-
-  async didLoad() {}
-
   async willReady() {
     // All plugins have started, can do some thing before app ready
     this.app.apm = apm;
     // 设置错误日志的传输通道。所有logger.error的错误，都会发送到APM上面。
-    app.getLogger('errorLogger').set('apmError', new ApmErrorTransport({ level: 'ERROR' }, apm));
-  }
-
-  async didReady() {}
-
-  async serverDidReady() {}
-
-  async beforeClose() {
-    // Do some thing before app close.
+    this.app.getLogger('errorLogger').set('apmError', new ApmErrorTransport({ level: 'ERROR' }, apm));
   }
 }
 
