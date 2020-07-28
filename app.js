156ba76f1c45f349ad87e3ff815fac4100557247
ddz <ddzyan@163.com>
2020-07-28 11:46:48 +0800

    修复app.js挂载方式

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ class AppBootHook {
 
   async willReady() {
     // All plugins have started, can do some thing before app ready
-    this.app.addSingleton('apm', apm);
+    this.app.apm = apm;
     // 设置错误日志的传输通道。所有logger.error的错误，都会发送到APM上面。
     this.app.getLogger('errorLogger').set('apmError', new ApmErrorTransport({ level: 'ERROR' }, apm));
   }
